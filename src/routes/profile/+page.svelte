a9e2900a745fca42d9b0562c9c2300065e09befb
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2023-02-28 12:12:49 +0530

    updeated profile

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { MoreHorizontal } from 'lucide-svelte';
 	import { onMount } from 'svelte';
 	import { supabase } from '$lib/supabaseClient';
 	import { page } from '$app/stores';
@@ -26,7 +27,7 @@
 				.single();
 
 			if (data) {
-				console.log(data)
+				console.log(data);
 				username = data.username;
 				coverUrl = data.cover_url;
 				avatarUrl = data.avatar_url;
@@ -41,15 +42,44 @@
 			loading = false;
 		}
 	};
-
-
 </script>
 
-<div class="main py-12 px-8">
-	<h1>{username}</h1>
-	<img src={avatarUrl} alt="">
-	<h1>{coverUrl}</h1>
-	<a href="/profile/update">Update Profile</a>
+<div class="cover h-[350px] w-full relative">
+	<div class="cover-image h-full w-full object-cover">
+		<img
+			src={coverUrl}
+			class="h-full w-full object-cover brightness-75"
+			alt=""
+		/>
+	</div>
+	<div class="absolute flex items-end top-20 left-10 bottom-8 space-x-8 right-10">
+		<div class="avatar-image w-56 aspect-square rounded-full bg-zinc-900 truncate">
+			<img
+				src={avatarUrl}
+				class="w-full h-full object-cover"
+				alt=""
+			/>
+		</div>
+
+		<div class="info ">
+			<h1 class="text-sm font-semibold px-2">PROFILE</h1>
+			<h1
+				class="text-8xl text-start font-bold line-clamp-3 overflow-hidden tracking-tight pt-1 pb-4"
+			>
+				{username}
+			</h1>
+			<h1 class="text-sm font-semibold px-2">0 Likes . 0 Following</h1>
+		</div>
+	</div>
 </div>
 
+<div class="main px-8 pt-8 space-y-10">
+	<div class="icon">
+		<MoreHorizontal size="32" color="#A5A5A5" />
+	</div>
 
+	<div class="Profile  space-y-4">
+		<h1 class="text-2xl">Profile</h1>
+		<h1 class="text-zinc-500">Nothing to show for now</h1>
+	</div>
+</div>
