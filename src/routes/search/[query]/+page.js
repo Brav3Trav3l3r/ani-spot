48b527c917012dbc1de21995d1ae05d9afeb2f28
BraveTraveller <yashgajbhiye10@gmail.com>
2023-03-03 14:17:48 +0530

    ;

--- src/routes/search/[query]/+page.js
+++ src/routes/search/[query]/+page.js
@@ -1,5 +1,5 @@
 export const load = async({params, fetch})=>{
-    const searchRes = await fetch(` https://cors.delusionz.xyz/https://api-consumet-rust.vercel.app/meta/anilist/${params.query}`)
+    const searchRes = await fetch(`https://api-consumet-rust.vercel.app/meta/anilist/${params.query}`)
     const searchJson = await searchRes.json()
 
     return{
